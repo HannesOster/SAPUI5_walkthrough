44f079c42991ed108b2e78ec078aba9eee74b802
Johannes Osterkamp <johannes.osterkamp@gmx.net>
2023-08-19 17:11:21 +0200

    fixed typos

--- webapp/index.js
+++ webapp/index.js
@@ -1,7 +1,7 @@
-sap.ui.define(["sap/ui/core/mvc/XLMView"], function (XMLView) {
+sap.ui.define(["sap/ui/core/mvc/XMLView"], function (XMLView) {
   "use strict";
   XMLView.create({
-    viewName: "sap.ui.demo.walktrough.view.App,",
+    viewName: "sap.ui.demo.walkthrough.view.App",
   }).then(function (oView) {
     oView.placeAt("content");
   });
